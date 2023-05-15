6a3fc9174496954ca8a4fc03a4cc686d9feb5a64
Jose Ramirez <19161388@itoaxaca.edu.mx>
2023-05-15 16:37:11 -0600

    Corregi bug cuando no hay tarjetas

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -5,12 +5,16 @@ let index = 1;
 function mostrarTarjetaSiguiente(event) {
     event.preventDefault();
     const tarjetaActual = document.getElementById('card' + index);
-    tarjetaActual.classList.add('d-none');
-
+    
     index++;
     if (index > 3) index = 1;
-
+    
     const nuevaTarjeta = document.getElementById('card' + index);
+    if(!nuevaTarjeta) {
+        index--
+        return
+    }
+    tarjetaActual.classList.add('d-none');
     nuevaTarjeta.classList.remove('d-none');
     nuevaTarjeta.classList.add('d-block');
 }
@@ -18,12 +22,16 @@ function mostrarTarjetaSiguiente(event) {
 function mostrarTarjetaAnterior(event) {
     event.preventDefault();
     const tarjetaActual = document.getElementById('card' + index);
-    tarjetaActual.classList.add('d-none');
 
     index--;
     if (index < 1) index = 3;
 
     const nuevaTarjeta = document.getElementById('card' + index);
+    if(!nuevaTarjeta) {
+        index++
+        return
+    }
+    tarjetaActual.classList.add('d-none');
     nuevaTarjeta.classList.remove('d-none');
     nuevaTarjeta.classList.add('d-block');
 }
